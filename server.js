fdcccd03d48f7098b1660e886f6d1ee044c0091f
Manthan <manthansoul@gmail.com>
2023-07-16 00:18:44 +0530

    create Express Server for this App

---
+++ server.js
@@ -0,0 +1,48 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import renderApp from "./dist/server/ServerApp.js";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const PORT = process.env.PORT || 3001;
+
+const html = fs
+  .readFileSync(path.resolve(__dirname, "./dist/client/index.html"))
+  .toString();
+
+const parts = html.split("not rendered");
+
+const app = express();
+
+app.use(
+  "/assets",
+  express.static(path.resolve(__dirname, "./dist/client/assets"))
+);
+app.use((req, res) => {
+  res.write(parts[0]);
+  const stream = renderApp(req.url, {
+    onShellReady() {
+      // if it is the crawler, do nothing here
+      stream.pipe(res);
+    },
+    onShellError() {
+      // do error handling
+    },
+    onAllReady() {
+      // if it is the crawler
+      // stream.pipe(res)
+
+      // last thing to write
+      res.write(parts[1]);
+      res.end();
+    },
+    onError(err) {
+      console.error(err);
+    },
+  });
+});
+
+console.log(`listening on http://localhost:${PORT}`);
+app.listen(PORT);
